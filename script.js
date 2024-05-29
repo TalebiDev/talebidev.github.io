bc15253566c8cc4d06da0b4965a7ea33d64e36cb
Mohammad Talebi <80977793+TalebiDev@users.noreply.github.com>
2024-05-29 11:23:33 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: [" Junior Backend Developer", "Python / Django"],
+    typeSpeed: 75,
+    backSpeed: 25,
+    backDelay: 1000,
+    loop: true
+})

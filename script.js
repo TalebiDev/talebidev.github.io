e316748d6dd48480683815c0511fca1078324a72
Mohammad Talebi <80977793+TalebiDev@users.noreply.github.com>
2025-01-01 20:07:37 +0330

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: [" Junior Backend Developer", "Python / Django"],
-    typeSpeed: 75,
-    backSpeed: 25,
-    backDelay: 1000,
-    loop: true
-})
